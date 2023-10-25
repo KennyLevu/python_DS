63a0f515308694b4632bef9c04cb5186e2ad08c5
Kenny <kennylevu1@gmail.com>
2023-10-25 15:20:04 -0700

    added comments to insertion sort

--- sorting algorithms/insertionsort.py
+++ sorting algorithms/insertionsort.py
@@ -10,12 +10,21 @@
 # Auxiliary Space: O(1)
 
 def insertion_sort(unsorted_list):
+    # start from the second element since the first element is considered sorted
     for i in range(1, len(unsorted_list)):
+        # variable keeps track of previous element's index
         j = i-1
+        # stores element to be inserted
         nxt_element = unsorted_list[i]
+
+        # inserts current element among the previous elements
+        # shift larger elements to the right until we find the correct position
         while (unsorted_list[j] > nxt_element) and (j >= 0):
+            # shift larger element to the right
             unsorted_list[j + 1] = unsorted_list[j]
+            # move one position to the left
             j=j-1
+        # insert current element into correct position in the sorted part of the list
         unsorted_list[j + 1] = nxt_element
     return unsorted_list
 
